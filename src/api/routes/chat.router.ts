d78ed410eaaa9dbd71eefa8aefe3ccf3a0a8eb0d
unknown <chavdaprayag0@gmail.com>
2026-09-06 01:57:26 +0530

    fix(router): move newsletterMetadata higher in router chain

--- src/api/routes/chat.router.ts
+++ src/api/routes/chat.router.ts
@@ -60,6 +60,16 @@ export class ChatRouter extends RouterBroker {
           return res.status(HttpStatus.BAD_REQUEST).json(error);
         }
       })
+      .post(this.routerPath('newsletterMetadata'), ...guards, async (req, res) => {
+        const response = await this.dataValidate<InstanceDto>({
+          request: req,
+          schema: null,
+          ClassRef: InstanceDto,
+          execute: (instance) => chatController.getNewsletterMetadata(instance, req.body?.jid || req.body?.number),
+        });
+
+        return res.status(HttpStatus.OK).json(response);
+      })
       .post(this.routerPath('markMessageAsRead'), ...guards, async (req, res) => {
         const response = await this.dataValidate<ReadMessageDto>({
           request: req,
